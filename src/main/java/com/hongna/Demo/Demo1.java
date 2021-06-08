1f19c091fffe6f0ac262ffdb1b06d7da2efca64a
Wonderchn <46928995+Wonderchn@users.noreply.github.com>
2021-06-08 11:12:04 +0800

    TEST

---
+++ src/main/java/com/hongna/Demo/Demo1.java
@@ -0,0 +1,17 @@
+package com.hongna.Demo;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        System.out.println(obj.getClass().getClassLoader());
+        //结果为Null 证明这个类是在根加载器中进行处理的
+
+        Demo1 demo1 = new Demo1();
+        System.out.println(demo1.getClass().getClassLoader());
+        System.out.println(demo1.getClass().getClassLoader().getParent());
+        System.out.println(demo1.getClass().getClassLoader().getParent().getParent());
+        //自己创建的类是通过AppClassLoader 来实现类的加载的
+        //这里会涉及一个 类的双亲委托机制
+
+    }
+}
